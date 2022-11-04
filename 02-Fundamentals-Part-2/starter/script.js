160a9aae2318d9be31796e4b60084caae9bd4b36
JordanTimberlake <u21565882@tuks.co.za>
2022-11-04 09:36:06 +0200

    Done up to First challenge

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,145 @@
+'use strict';
+
+/*
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true; // s in drivers is ommited
+if (hasDriversLicense) console.log("I can drive! :D");
+
+
+// Functions
+
+function logger(stringval) {
+    console.log(stringval);
+}
+
+// Calling / Running / Invoking the function
+logger("My");
+logger("name");
+logger("is");
+logger("Jordan");
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+let AppleJuice = fruitProcessor(3, 0);
+
+console.log(AppleJuice);
+
+const appleOrangeJuice = fruitProcessor(2,4);
+
+console.log(appleOrangeJuice);
+
+// function declarations vs expressions
+
+// function declaration
+function calcAge1(BirthYear){
+    return 2022 - BirthYear;
+}
+
+console.log(calcAge1(2001));
+
+// function expression
+const calcAge2 = function (BirthYear){
+    return 2022 - BirthYear;
+}
+
+console.log(calcAge2(2001));
+
+console.log(calcAge1(2001),calcAge2(2001));
+
+// Arrow Functions
+
+const calcAge3 = birthYear => 2022 - birthYear;
+console.log(calcAge3(2001));
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2022 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(2001, "Jordan"));
+console.log(yearsUntilRetirement(1990, "Bob"));
+
+
+const firstName = fN => {
+    return fN;
+}
+
+const secondName = sN => {
+    return sN;
+}
+
+const fullName = (fN,sN) => {
+    return `Your full name is ${fN} ${sN}`;
+}
+
+console.log(fullName(firstName("Jordan"), secondName("Timberlake")));
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice made with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
+
+const calcAge = function(birthYear){
+    return 2022 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    
+    if(retirement > 0){
+        return `${firstName} retires in ${retirement} years.`;
+    }else {
+        return `${firstName} has already retired.`;
+    }
+    
+}
+
+console.log(yearsUntilRetirement(2001, "Jordan"));
+console.log(yearsUntilRetirement(1950, "Mike"));
+
+// Coding challenge 1
+
+const calcAverage = (s1, s2, s3) => Math.trunc((s1 + s2 + s3) / 3);
+
+const checkWinner = (avgt1, t1, avgt2, t2) => {
+    if (avgt1 >= 2 * avgt2) {
+        return `${t1} win (${avgt1} vs ${avgt2}).`;
+    } else if (avgt2 >= 2 * avgt1) {
+        return `${t2} win (${avgt2} vs ${avgt1}).`;
+    } else {
+        return `No team wins.`;
+    }
+}
+
+const d = "Dolphins";
+const k = "Koalas";
+
+console.log(calcAverage(44, 23, 71));
+console.log(calcAverage(85, 54, 41));
+console.log(checkWinner(calcAverage(44, 23, 71), d, calcAverage(85, 54, 41), k));
+
+console.log(calcAverage(85, 52, 41));
+console.log(calcAverage(23, 34, 27));
+console.log(checkWinner(calcAverage(85, 52, 41), d, calcAverage(23, 34, 27), k));
+*/
+
+// Arrays
+
+
