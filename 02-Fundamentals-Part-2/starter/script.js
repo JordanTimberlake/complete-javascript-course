2edc5a086b5a9e8f066c71d8508abe4e4171501d
JordanTimberlake <u21565882@tuks.co.za>
2022-11-04 10:42:35 +0200

    Everything before Challenge#3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -138,8 +138,128 @@ console.log(checkWinner(calcAverage(44, 23, 71), d, calcAverage(85, 54, 41), k))
 console.log(calcAverage(85, 52, 41));
 console.log(calcAverage(23, 34, 27));
 console.log(checkWinner(calcAverage(85, 52, 41), d, calcAverage(23, 34, 27), k));
-*/
 
 // Arrays
 
+const friend1 = 'Micheal';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
 
+const friends = ['Micheal','Steven','Peter'];
+console.log(friends);
+console.log(friends[0]);
+const years = new Array(1991,1984,2001,2020);
+console.log(years);
+years.push(2022);
+console.log(years);
+console.log(years[0]);
+
+for (let i = 0; i < friends.length; i=i+1){
+    console.log(friends[i]);
+}
+
+// Coding challenge 2
+
+const bills = new Array(125, 555, 44);
+const total = new Array();
+const total1 = new Array();
+
+const calcTip = billAmount => {
+    if (billAmount >= 50 && billAmount <= 300) {
+        return billAmount * 1.15;
+    } else {
+        return billAmount * 1.2;
+    }
+}
+
+const calcTip1 = bill => bill >= 50 && bill <= 300 ? bill*1.15 : bill*1.2;
+
+for (let i = 0; i < bills.length; i = i + 1) {
+    total.push(calcTip(bills[i]));
+    console.log(total[i]);
+}
+
+for (let i = 0; i < bills.length; i = i + 1) {
+    total1.push(calcTip1(bills[i]));
+    console.log(total1[i]);
+}
+
+// OBJECTS in javascript
+
+const jordan = {
+    firstName: 'Jordan',
+    lastName: 'Timberlake',
+    age: 2022 - 2001,
+    job: 'Student',
+    friends: ['Imtiyaaz', 'Connor', 'Cals', 'Dwayne', 'Ruan', 'Kevin']
+};
+
+jordan.location = 'South Africa'
+console.log(jordan);
+
+console.log(jordan.lastName);
+console.log(jordan['lastName']); // bracket notation
+
+const namekey = 'Name';
+
+console.log(jordan['first' + namekey]);
+console.log(jordan['last' + namekey]);
+
+const interestedIn = prompt('What do you want to know about Jordan? Choose between firstName, lastName, age, job and friends?');
+
+if(jordan[interestedIn]){
+    alert(jordan[interestedIn]);
+}else {
+    prompt('Wrong request. Choose between firstName, lastName, age, job and friends?');
+}
+
+console.log(`${jordan.firstName} has ${jordan.friends.length} friends, and his best 
+friend is called ${jordan.friends[0]}`);
+
+*/
+
+const jordan = {
+    firstName: 'Jordan',
+    lastName: 'Timberlake',
+    birthYear: 2001,
+    job: 'Student',
+    location: 'South Africa',
+    friends: ['Imtiyaaz', 'Connor', 'Cals', 'Dwayne', 'Ruan', 'Kevin'],
+    hasDriversLicence: true,
+    // calcAge: function(birthYear){
+    //     return 2022 - birthYear;
+    // }
+    // calcAge: function(){
+    //     return 2022 - this.birthYear;
+    // }
+    calcAge: function(){
+        jordan.age = 2022 - this.birthYear;
+        return this.age;
+    },
+    summary: function(){
+        return `${this.firstName} ${this.lastName} is a ${this.calcAge()}-year-old
+        ${this.job} and has ${this.hasDriversLicence ? 'a' : 'no'} drivers license.`
+    },
+    addFriend: function(newFriend){
+        if(this.friends.indexOf(newFriend) == -1){
+            // need to add the friend into the array
+            this.friends.push(newFriend);
+        }else{
+            console.log(`${newFriend} is already in ${jordan.firstName}'s friend list`);
+        }
+    }
+};
+
+console.log(jordan.calcAge());
+console.log(jordan.age);
+console.log(jordan.summary());
+
+jordan.addFriend('Jake');
+for (let i = 0; i < jordan.friends.length; i = i + 1) {
+    console.log(jordan.friends[i]);
+}
+
+jordan.addFriend('Ruan');
+for (let i = 0; i < jordan.friends.length; i = i + 1) {
+    console.log(jordan.friends[i]);
+}
